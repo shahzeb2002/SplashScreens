b3e3ffca3320d2ee8affc4e5a32c7ef30b0b2c3c
Shah Zeb <138509711+shahzeb2002@users.noreply.github.com>
2025-11-13 20:31:41 +0500

    ,

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screens/shopify_splash.dart';
+import 'package:splash_screens/splash_2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: OnboardingSplash(),
+    );
+  }
+}
